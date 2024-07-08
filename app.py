cbd169d9b371a6816cf9a88de965585403fb38c4
francorp1993 <110462897+francorp1993@users.noreply.github.com>
2024-07-07 23:21:27 -0300

    correcion app (sin paw)

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_login import LoginManager
-from componentes.modelos import Usuario  # Asegúrate de que esta ruta de importación sea correcta
+from componentes.modelos import Usuario 
 
 app = Flask(__name__)
 app.json.ensure_ascii = False
 
 # Configuración CORS para todas las rutas de la aplicación
-CORS(app)
-
-# Configuración CORS específica para rutas bajo /api
-# CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app, resources={r"/api/*": {"origins": "http://127.0.0.1:5500"}})
 
 # Utiliza una variable de entorno para la clave secreta en producción
 app.secret_key = 'admin'
@@ -21,17 +18,18 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return Usuario.query.get(int(user_id))  # Asumiendo que la clase Usuario tiene un campo de clave primaria 'id'
+    return Usuario.query.get(int(user_id)) 
+
+# Ejemplo de una ruta API para 'especialidades'
+@app.route('/api/especialidades', methods=['GET'])
+def get_especialidades():
+    # Aquí iría tu lógica para obtener las especialidades
+    especialidades = ["Cardiología", "Dermatología", "Pediatría"]
+    return jsonify(especialidades)
 
 # Importar las vistas/APIs
 from componentes.vistas_api import *
 
-# Configuración de SQLAlchemy si estás utilizando
-# from flask_sqlalchemy import SQLAlchemy
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'tu-uri-de-base-de-datos'
-# db = SQLAlchemy(app)
-# db.init_app(app)
-
 # La siguiente parte se ejecuta solo en desarrollo, no en producción
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
